f45524083ef256784470264b9fb9bfed666212d6
lilyhoratio <lilyhzhou@gmail.com>
2019-07-18 10:09:34 -0700

    hrefs and color to nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,9 @@ function Header() {
     return (
         <nav className="nav-container">
             <img width="180px" src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png"></img>
-            <a>Generate a random photo</a>
-            <a>Submit a photo</a>
+            {/* How to get this to work? */}
+            <a href="#">Generate a random photo</a>
+            <a href="#">Submit a photo</a>
         </nav>
     )
 }
